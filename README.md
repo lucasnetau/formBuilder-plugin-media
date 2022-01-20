b5532d3ed4ded24aac192ce22d7f4be7e86086e7
James Lucas <james@lucas.net.au>
2022-01-20 16:27:11 +1100

    Initial import of media Control plugin

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# formBuilder-plugin-media
-Plugin for kevinchappell/formBuilder to render media (image/video/audit) on forms
+# Media (Image/Video/Audio) plugin for kevinchappell/formBuilder
+![GitHub](https://img.shields.io/github/license/lucasnetau/formBuilder-plugin-media)
+
+A plugin to embed images, video, or audio into a formBuilder form.
+
+## Features
+* Renders HTML5 elements - Figure, Video, Audio
+* Supports embedding via:
+  * URL
+  * DataURI
+  * File Upload
+
+## Installation
+
+Include the plugin after formBuilder/formRender JS include
+
+```<script src="image.js"></script>```
+
+## Configuration
+Un-used attributes "placeholder" and "required" are presented in the edit panel. These can be removed via configuring typeUserDisabledAttrs in formBuilder
+
+```javascript
+typeUserDisabledAttrs: {
+    media: [
+        'placeholder',
+        'required',
+    ]
+}
+```
+
+##Notes on File Upload support
+* Media uploads are converted to DataURIs in the browser and stored in the **src** field attribute. It is recommended that when processing the formBuilder template on the backend that the DataURI is uploaded (for example to a S3 bucket) and the **src** attribute is modified to point to the public URL.
+* For large files it is recommended to manually upload media and link to the public URL instead of using the file upload support
---
+++ media.js
@@ -0,0 +1,160 @@
+"use strict";
+/**
+ * This file is part of the Media (Image,Video,Audio) Element for formBuilder.
+ * https://github.com/lucasnetau/formBuilder-plugin-media
+ *
+ * (c) James Lucas <james@lucas.net.au>
+ *
+ * @license MIT
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+if (!window.fbControls) { window.fbControls = []; }
+window.fbControls.push(function media(controlClass) {
+    class controlMedia extends controlClass {
+
+        /**
+         * Load embedded Javascript
+         */
+        configure() {
+            const marker = 'controlMediaEmbedded';
+            const cache = window.fbLoaded.js; //Reuse the FormBuilder cache to ensure we only load the media control JS once
+            if (! cache.includes(marker)) {
+                $(document.body).on('change', '.form-builder .frm-holder .fld-media-file-upload', function() {
+                    const input = $(this);
+                    let reader = new FileReader();
+
+                    //Async read of the uploaded file and convert to a DateURI. Detect mimetype and adjust mimetype attribute and control Subtype
+                    reader.addEventListener("load", function () {
+                        const regexp = /^data:((?:\w+\/(?:(?!;).)+)?)/;
+                        const srcElement = input.closest('.frmb.stage-wrap').find('.fld-src');
+                        const dataUri = this.result;
+                        const mediatype = dataUri.match(regexp);
+                        if (null !== mediatype) {
+                            input.closest('.frmb.stage-wrap').find('.fld-mimetype').val(mediatype[1]);
+
+                            let pluginSubtype;
+                            if (mediatype[1].startsWith('image/')) {
+                                pluginSubtype = 'image';
+                            } else if (mediatype[1].startsWith('video/')) {
+                                pluginSubtype = 'video';
+                            } else {
+                                pluginSubtype = 'audio';
+                            }
+                            input.closest('.frmb.stage-wrap').find('.fld-subtype').val(pluginSubtype);
+                        }
+                        srcElement.val(dataUri).trigger('change');
+                        input.val("");
+                    });
+                    reader.readAsDataURL(input[0].files[0]);
+                });
+                cache.push(marker);
+            }
+        }
+
+        /**
+         * Class configuration - return the icons & label related to this control
+         * @return {object} definition
+         */
+        static get definition() {
+            return {
+                icon: '🖼️',
+                i18n: {
+                    default: 'Media',
+                },
+                defaultAttrs: {
+                    'className': {
+                        label: "Class",
+                        value: 'img-fluid',
+                        type: 'text',
+                    },
+                    'description': {
+                        label: "Help Text",
+                        value: '',
+                        type: 'text',
+                    },
+                    'src': {
+                        label: "Src",
+                        value: '',
+                        type: 'textarea', //text inputs do not handle large data URI strings, need to use textarea to ensure the browser doesn't hang
+                    },
+                    'mimetype': {
+                        label: "Mime Type",
+                        value: '',
+                        type: 'text',
+                        description: 'Mimetype of Media',
+                        //readonly: true,
+                    },
+                    'media-file-upload': {
+                        label: "File",
+                        value: '',
+                        type: 'file',
+                        description: 'Upload a media file (Image, Audio, Video)',
+                        accept:"image/*,video/mp4,video/x-m4v,video/*,audio/x-m4a,audio/*",
+                    },
+                    'width': {
+                        label: "Width",
+                        value: '200',
+                        type: 'text',
+                    },
+                    'height': {
+                        label: "Height",
+                        value: 'auto',
+                        type: 'text',
+                    },
+                    'subtype': {
+                        label: 'Media Type',
+                        options: {
+                            'image': 'Image',
+                            'video': 'Video',
+                            'audio': 'Audio',
+                        },
+                    },
+                },
+            };
+        }
+
+        /**
+         * Build the HTML5 attribute for the specified media type
+         * @return {Object} DOM Element to be injected into the form.
+         */
+        build() {
+            const {...attrs} = this.config;
+            delete(attrs.type);
+
+            switch(this.subtype) {
+                case 'image':
+                    let caption = this.markup('figcaption', attrs.label, {});
+                    let img = this.markup('img', null, attrs);
+                    let figure = this.markup('figure', [img, caption,], attrs);
+                    return {
+                        field: figure,
+                        layout: 'noLabel',
+                    };
+
+                case 'video':
+                    attrs.controls = true;
+                    attrs.controlsList = "nodownload";
+                    let videoSource = this.markup('source', null, {src: attrs.src, type: attrs.mimetype,});
+                    return this.markup('video', [videoSource, '<p>Your browser does not support HTML5 video</p>',], attrs);
+
+                case 'audio':
+                    attrs.controls = true;
+                    let audioSource = this.markup('source', null, {src: attrs.src, type: attrs.mimetype,});
+                    return this.markup('audio', [audioSource, '<p>Your browser does not support HTML5 audio</p>',], attrs);
+            }
+        }
+
+        /**
+         * onRender callback
+         */
+        onRender() {
+
+        }
+    }
+
+    // register this control for the following types & text subtypes
+    controlClass.register('media', controlMedia);
+    controlClass.register(['image','video','audio',], controlMedia, 'media');
+    return controlMedia;
+});
